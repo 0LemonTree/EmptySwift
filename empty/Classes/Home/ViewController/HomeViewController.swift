786e114676b10b742e4641580cd7c0e29112fb68
0LemonTree <abcxdx@163.com>
2019-01-11 17:01:10 +0800

    添加一些常用文件夹

--- empty/Classes/Home/ViewController/HomeViewController.swift
+++ empty/Classes/Home/ViewController/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  empty
 //
 //  Created by apple on 2019/1/11.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
-
-
 }
-
